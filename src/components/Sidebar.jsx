e90986c70a3078e0a2bf26dcf77ffdefec8118ac
hassanwaqa <hassanwaqa277@gmail.com>
2024-03-21 10:10:07 +0500

    Initial Setup

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { HiAdjustments, HiDotsHorizontal, HiOutlineHeart, HiLogout, HiOutlineHashtag, HiOutlineHome, HiOutlineMenu, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
+import { RiCloseLine } from 'react-icons/ri';
+
+import { logo } from '../assets/';
+
+const links = [
+  { name: 'Discover', to: '/', icon: HiOutlineHome },
+  { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
+  { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
+  { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
+];
+
+const discover = [
+  { name: 'Home', to: '/', icon: HiOutlineHome },
+  { name: 'Explore', to: '/explore', icon: HiOutlinePhotograph },
+];
+
+const library = [
+  { name: 'Favourite', to: '/favourite', icon: HiOutlineHeart },
+  { name: 'Playlits', to: '/playlists', icon: HiAdjustments },
+];
+
+const more = [
+  { name: 'Settings', to: '/settings', icon: HiDotsHorizontal },
+  { name: 'Account', to: '/account', icon: HiOutlineUserGroup },
+  { name: 'Logout', to: '/logout', icon: HiLogout },
+];
+
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10 ml-10 ">
+    <h6>Discover</h6>
+    {discover.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+    <h6>Library</h6>
+    {library.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+    <h6>More</h6>
+    {more.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[300px] py-10 px-4 bg-[#fff] shadow-lg translate-x-1">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+
+      {/* Mobile sidebar */}
+      <div className="absolute md:hidden block top-6 right-3">
+        {!mobileMenuOpen ? (
+          <HiOutlineMenu className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(true)} />
+        ) : (
+          <RiCloseLine className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(false)} />
+        )}
+      </div>
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-white/10 to-blue-500 backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
